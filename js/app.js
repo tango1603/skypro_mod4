fcd63d3aedd3b4ba625b979da006e1e55c2fc678
Ilya Bashilov <tango1603@yandex.ru>
2023-05-01 01:20:12 +0400

    add start game logic

--- js/app.js
+++ js/app.js
@@ -1,23 +1,49 @@
+import { Game } from "./game.js";
 
 class App {
   constructor(parentNode) {
     console.log("Game started", this);
     this.parentNode = parentNode;
-    
-    this.server = new Server();  
-    
-    
 
-    this.difficultyLevel=null;
-    this.timeInGame=null;
+    this.difficultyLevel = null;
+    this.timeInGame = null;
     this.cards = [];
     this.selectedCards = [];
 
+    this.difficultyLevelBtns = parentNode.querySelectorAll(
+      ".difficulty-level__item"
+    );
     
+    for (const btn of this.difficultyLevelBtns) {
+      btn.addEventListener("click", () => {
+        this.difficultyLevel = btn.getAttribute("data-difficulty");
+        console.log("difficulty-level__item click", this.difficultyLevel);
+
+        for (const cbtn of this.difficultyLevelBtns) {
+          cbtn.classList.remove("btn_selected");
+        }
+
+        btn.classList.add("btn_selected");
+      });
+    }
+
+
+    this.startBtn = parentNode.querySelector(".btn");
+    this.startBtn.addEventListener("click", () => {     
+      if (!this.difficultyLevel) return;
+      
+      this.game = new Game(this);
+      this.hide();
+    });
   }
 
   hide() {
-    this.parentNode.firstChild.remove();
+    console.log("hide()");
+    this.parentNode.firstElementChild.remove();
+  }  
+
+  render(){
+    //TODO: реализовать рендер по шаблону
   }
 }
 
---
+++ js/game.js
@@ -0,0 +1,16 @@
+export class Game {
+  constructor(state) {
+    this.state = state;    
+    this.render();
+  }
+
+  render() {
+    //TODO: реализовать рендер по шаблону
+    console.log("renderGame", this.state);
+
+
+
+
+
+  }
+}
