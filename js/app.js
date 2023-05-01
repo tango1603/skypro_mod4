7fe6ad677dc91f3c9b5fb1a40a820ab6a5a5a229
Ilya Bashilov <tango1603@yandex.ru>
2023-05-01 15:04:57 +0400

    add linters, prettier,husky, fix finded linter bugs, test husky

--- js/app.js
+++ js/app.js
@@ -1,55 +1,58 @@
-import { Game } from "./game.js";
+import { Game } from './game.js';
 
 class App {
-  constructor(parentNode) {
-    console.log("Game started", this);
-    this.parentNode = parentNode;
+    constructor(parentNode) {
+        console.log('Game started', this);
+        this.parentNode = parentNode;
 
-    this.difficultyLevel = null;
-    this.timeInGame = null; //TODO: реализовать getTimeInGame()
-    this.cards = []; //TODO: реализовать getRandomCards(difficultyLevel)
-    this.selectedCards = [];
+        this.difficultyLevel = null;
+        this.timeInGame = null; //TODO: реализовать getTimeInGame()
+        this.cards = []; //TODO: реализовать getRandomCards(difficultyLevel)
+        this.selectedCards = [];
 
-    render();
+        this.render();
 
-    this.difficultyLevelBtns = parentNode.querySelectorAll(
-      ".difficulty-level__item"
-    );
+        this.difficultyLevelBtns = parentNode.querySelectorAll(
+            '.difficulty-level__item'
+        );
 
-    for (const btn of this.difficultyLevelBtns) {
-      btn.addEventListener("click", () => {
-        this.difficultyLevel = btn.getAttribute("data-difficulty");
-        console.log("difficulty-level__item click", this.difficultyLevel);
+        for (const btn of this.difficultyLevelBtns) {
+            btn.addEventListener('click', () => {
+                this.difficultyLevel = btn.getAttribute('data-difficulty');
+                console.log(
+                    'difficulty-level__item click',
+                    this.difficultyLevel
+                );
 
-        for (const cbtn of this.difficultyLevelBtns) {
-          cbtn.classList.remove("btn_selected");
+                for (const cbtn of this.difficultyLevelBtns) {
+                    cbtn.classList.remove('btn_selected');
+                }
+
+                btn.classList.add('btn_selected');
+            });
         }
 
-        btn.classList.add("btn_selected");
-      });
+        this.startBtn = parentNode.querySelector('.btn');
+        this.startBtn.addEventListener('click', () => {
+            if (!this.difficultyLevel) return;
+
+            this.game = new Game(this);
+            this.hide();
+        });
     }
 
-    this.startBtn = parentNode.querySelector(".btn");
-    this.startBtn.addEventListener("click", () => {
-      if (!this.difficultyLevel) return;
+    hide() {
+        console.log('hide()');
+        this.parentNode.firstElementChild.remove();
+    }
 
-      this.game = new Game(this);
-      this.hide();
-    });
-  }
-
-  hide() {
-    console.log("hide()");
-    this.parentNode.firstElementChild.remove();
-  }  
-
-  render(){
-    return 1;
-    //TODO: реализовать рендер по шаблону
-  }
+    render() {
+        return 1;
+        //TODO: реализовать рендер по шаблону
+    }
 }
 
-document.addEventListener("DOMContentLoaded", onContentLoaded);
-function onContentLoaded(e) {
-  window.application = new App(document.querySelector("#app"));
+document.addEventListener('DOMContentLoaded', onContentLoaded);
+function onContentLoaded() {
+    window.application = new App(document.querySelector('#app'));
 }
